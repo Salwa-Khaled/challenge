731b8eb68393d460eb8199dee3d1a2258ba9f658
SalwaMaktari <97165291+SalwaALmaktari@users.noreply.github.com>
2022-01-12 17:14:27 +0300

    Add files via upload

---
+++ Q1_5.py
@@ -0,0 +1,14 @@
+total = 0
+while total <= 50 :
+    n = input("number : ")
+    if number.isdigit() :
+        number = int(number)
+        total = total + n
+        print("Total is : ", total)
+    else :
+        print( ' Error , Try again')
+        
+        
+        
+    
+    
